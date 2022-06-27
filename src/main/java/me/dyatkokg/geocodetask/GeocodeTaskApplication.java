805ae5cab4954b57946b0064279d32f3664fe62b
dyatkokg <dyatkokg@gmail.com>
2022-06-27 21:49:10 +0400

    first commit

---
+++ src/main/java/me/dyatkokg/geocodetask/GeocodeTaskApplication.java
@@ -0,0 +1,13 @@
+package me.dyatkokg.geocodetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeocodeTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeocodeTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/dyatkokg/geocodetask/GeocodeTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dyatkokg.geocodetask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeocodeTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
